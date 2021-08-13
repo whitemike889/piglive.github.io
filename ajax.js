71bdcb5483945c6ac91e8ebd45d841cadc6aa9f2
cdn2021 <78522762+cdn2021@users.noreply.github.com>
2021-08-13 10:46:54 +0800

    Add files via upload

--- ajax.js
+++ ajax.js
@@ -17,3 +17,22 @@ function ajax(url,responseType = "text"){
 	});
 };
 		
+function upload_data(url,data,token = null,sha = null){
+	if (window.XMLHttpRequest)
+		var xhr = new XMLHttpRequest();
+	else
+		var xhr = new ActiveXObject("Microsoft.XMLHTTP");
+	xhr.open("PUT",url,true);
+	if (token)
+		xhr.setRequestHeader("Authorization","token " + token);
+	return new Promise(function(resolve,reject){
+		xhr.send(JSON.stringify({content:btoa(data),message:"PigLive",sha:sha}));
+		xhr.onload = function(){
+			resolve([xhr.response,xhr.status]);
+		};
+		xhr.onerror = function(){
+			reject(xhr.status);	
+		};
+		xhr.onabort = reject;
+	});
+};
